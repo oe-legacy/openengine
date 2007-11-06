3ede3fe9d69fb2c96889c8f17a4369122e2ab37c
Anders Bach Nielsen <abachn@daimi.au.dk>
2007-11-06 17:22:30 +0100

    Added option to make.sh to create projects and extensions
    darcs-hash:20071106162230-35f4d-e3383c44dda505eeb6828ea85effaf605e4a80ea.gz

--- make.sh
+++ make.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 HELP="Small script to help working on the OpenEngine source.
 Some useful targets are:
-  all (default) -- set up the build system and compile the source
-  rebuild       -- remove the build system and rebuild from source
-  darcs <cmd>   -- run darcs command on all extensions, projects and openengine
-  test          -- run all tests
-  test-auto     -- run all automatic tests
-  test-manual   -- run all tests that need interaction
-  targets       -- list of make targets
-  doc           -- build the doxygen documentation (requires doxygen)
-  help          -- this message
-  [other]       -- forwarded to make in the build directory"
+  all (default)      -- set up the build system and compile the source
+  rebuild            -- remove the build system and rebuild from source
+  darcs <cmd>        -- run darcs command on all extensions, projects and openengine
+  deploy-proj <name> -- create new project projects/<name> from the ExampleProject
+  deploy-ext <name>  -- create new extension extensions/<name> from the ExampleExtension
+  test               -- run all tests
+  test-auto          -- run all automatic tests
+  test-manual        -- run all tests that need interaction
+  targets            -- list of make targets
+  doc                -- build the doxygen documentation (requires doxygen)
+  help               -- this message
+  [other]            -- forwarded to make in the build directory"
 
 # check the current working dir is the oe root.
 if [ ! -f `pwd`/make.sh ]; then
@@ -30,6 +32,20 @@ elif [ "$1" = "help" ]; then
     exit
 elif [ "$1" = "targets" ]; then
     CMD="help"
+elif [ "$1" = "deploy-proj" ]; then
+    NAME=$2
+    echo "*** Deploying new project to: projects/${NAME}"
+    cd projects
+    darcs get http://daimi.au.dk/~cgd/projects/ExampleProject $NAME
+    cd ..
+    exit
+elif [ "$1" = "deploy-ext" ]; then
+    NAME=$2
+    echo "*** Deploying new extension to: extensions/${NAME}"
+    cd extensions
+    darcs get http://daimi.au.dk/~cgd/extensions/ExampleExtension $NAME
+    cd ..
+    exit
 elif [ "$1" = "darcs" ]; then
     echo "**** $@ in OpenEngine"
     $@
