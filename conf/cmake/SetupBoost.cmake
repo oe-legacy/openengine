63a6ec6266374d83a88d5521c0320e964158bf8f
u040437 <u040437@daimi.au.dk>
2008-08-16 19:48:06 +0200

    Fixed cmake setup of boost for windows.
    darcs-hash:20080816174806-0dd6a-daf4797b2a3bf3e0a3b22168d2e4f05ec3b0f211.gz

--- conf/cmake/SetupBoost.cmake
+++ conf/cmake/SetupBoost.cmake
@@ -4,58 +4,48 @@ IF (Boost_FOUND)
 
    INCLUDE_DIRECTORIES(${Boost_INCLUDE_DIRS})
 
-   FIND_LIBRARY(BOOST_FILESYSTEM_LIB NAMES
-               boost_filesystem
-               libboost_filesystem-vc80-mt-1_33_1
-               libboost_filesystem-vc80-mt-1_34_1
-               PATHS
-               ${Boost_INCLUDE_DIRS}
-               ${Boost_INCLUDE_DIRS}/lib
-               )
-   IF (NOT BOOST_FILESYSTEM_LIB)
-      SET(Boost_FOUND 0)
-   ENDIF(NOT BOOST_FILESYSTEM_LIB)
+   # Debug mode libraries
+   IF(CMAKE_CXX_FLAGS_DEBUG)
+     FIND_LIBRARY(BOOST_FILESYSTEM_LIB NAMES
+                  boost_filesystem
+                  libboost_filesystem-vc80-mt-gd-1_33_1
+                  libboost_filesystem-vc80-mt-gd-1_34_1
+                  PATHS
+                  ${Boost_INCLUDE_DIRS}
+                  ${Boost_INCLUDE_DIRS}/lib
+                  )
+     FIND_LIBRARY(BOOST_SERIALIZATION_LIB NAMES
+                  boost_serialization
+                  libboost_serialization-vc80-mt-gd-1_33_1
+                  libboost_serialization-vc80-mt-gd-1_34_1
+                  PATHS
+                  ${Boost_INCLUDE_DIRS}
+                  ${Boost_INCLUDE_DIRS}/lib
+                  )
 
-   FIND_LIBRARY(BOOST_FILESYSTEM_LIB_gd NAMES
-               boost_filesystem
-               libboost_filesystem-vc80-mt-gd-1_33_1
-               libboost_filesystem-vc80-mt-gd-1_34_1
-               PATHS
-               ${Boost_INCLUDE_DIRS}
-               ${Boost_INCLUDE_DIRS}/lib
-               )
-   IF (NOT BOOST_FILESYSTEM_LIB_gd)
-      SET(Boost_FOUND 0)
-   ENDIF(NOT BOOST_FILESYSTEM_LIB_gd)
+   # Release mode libraries
+   ELSE(CMAKE_CXX_FLAGS_DEBUG)
+     FIND_LIBRARY(BOOST_FILESYSTEM_LIB NAMES
+                  boost_filesystem
+                  libboost_filesystem-vc80-mt-1_33_1
+                  libboost_filesystem-vc80-mt-1_34_1
+                  PATHS
+                  ${Boost_INCLUDE_DIRS}
+                  ${Boost_INCLUDE_DIRS}/lib
+                  )
+     FIND_LIBRARY(BOOST_SERIALIZATION_LIB NAMES
+                  boost_serialization
+                  libboost_serialization-vc80-mt-1_33_1
+                  libboost_serialization-vc80-mt-1_34_1
+                  PATHS
+                  ${Boost_INCLUDE_DIRS}
+                  ${Boost_INCLUDE_DIRS}/lib
+                  )
+   ENDIF(CMAKE_CXX_FLAGS_DEBUG)
 
-   FIND_LIBRARY(BOOST_TEST_LIB NAMES
-               boost_unit_test_framework
-               libboost_unit_test_framework-vc80-mt-1_33_1
-               libboost_unit_test_framework-vc80-mt-1_34_1
-               libboost_unit_test_framework-vc80-mt-gd-1_33_1
-               libboost_unit_test_framework-vc80-mt-gd-1_34_1
-               PATHS
-               ${Boost_INCLUDE_DIRS}
-               ${Boost_INCLUDE_DIRS}/lib
-               )
-   IF (NOT BOOST_TEST_LIB)
-      SET(Boost_FOUND 0) 
-   ENDIF(NOT BOOST_TEST_LIB)
-
-   FIND_LIBRARY(BOOST_SERIALIZATION_LIB NAMES
-               boost_serialization
-               # WINDOWS
-               libboost_serialization-vc80-mt-1_33_1
-               libboost_serialization-vc80-mt-1_34_1
-               libboost_serialization-vc80-mt-gd-1_33_1
-               libboost_serialization-vc80-mt-gd-1_34_1
-               PATHS
-               ${Boost_INCLUDE_DIRS}
-               ${Boost_INCLUDE_DIRS}/lib
-               )
-   IF (NOT BOOST_SERIALIZATION_LIB)
+   IF(NOT BOOST_FILESYSTEM_LIB OR NOT BOOST_SERIALIZATION_LIB)
       SET(Boost_FOUND 0)
-   ENDIF(NOT BOOST_SERIALIZATION_LIB)
+   ENDIF(NOT BOOST_FILESYSTEM_LIB OR NOT BOOST_SERIALIZATION_LIB)
 
 ENDIF (Boost_FOUND)
 
