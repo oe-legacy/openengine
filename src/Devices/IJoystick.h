86c00e7e0685d4b9509ca0592302490d93c21626
zerny <zerny@openengine.dk>
2008-09-23 16:39:07 +0200

    Formatting touch up.
    darcs-hash:20080923143907-50fb2-a364426e0e63ed8e7202bf1f5fcbcabebf86ef4a.gz

--- src/Devices/IJoystick.h
+++ src/Devices/IJoystick.h
@@ -1,6 +1,18 @@
+// Joystick interface.
+// -------------------------------------------------------------------
+// Copyright (C) 2007 OpenEngine.dk (See AUTHORS) 
+// 
+// This program is free software; It is covered by the GNU General 
+// Public License version 2 or any later version. 
+// See the GNU General Public License for more details (see LICENSE). 
+//--------------------------------------------------------------------
 
 #ifndef _OE_INTERFACE_JOYSTICK_H_
 #define _OE_INTERFACE_JOYSTICK_H_
+
+#include <Devices/Symbols.h>
+#include <Core/IModule.h>
+#include <Core/IEvent.h>
 
 namespace OpenEngine {
 namespace Devices {
@@ -13,42 +25,39 @@ namespace Devices {
  *
  * @struct JoystickState IJoystick.h Devices/IJoystick.h
  */
- struct JoystickState {
-   int axisState[28]; //!<optimized ie made for Logitech Rumblepad
-   JoystickButton buttons; //!< currently activated buttons
- JoystickState() : buttons(JBUTTON_NONE) {}
- };
+struct JoystickState {
+    int axisState[28]; //!<optimized ie made for Logitech Rumblepad
+    JoystickButton buttons; //!< currently activated buttons
+    JoystickState() : buttons(JBUTTON_NONE) {}
+};
 
 
-    struct JoystickAxisEventArg {
+struct JoystickAxisEventArg {
 	JoystickState state;
 	int axis;
 	int value;
-    };
+};
 
- /**
-  * Joystick button change event argument
-  * Sent to handlers listening on joystickUpEvent and joystickDownEvent.
-  *
-  * @struct JoystickButtonEventArg IJoystick.h Devices/IJoystick.h
-  */
- struct JoystickButtonEventArg {
-     enum JButtonEventType { UNKNOWN, PRESS, RELEASE };
-     JButtonEventType type;
-     JoystickState state; //!< current joystick state
-     JoystickButton button; //!< button that triggered the event
-     JoystickButtonEventArg() : button(JBUTTON_NONE) {}
- };
+/**
+ * Joystick button change event argument
+ * Sent to handlers listening on joystickUpEvent and joystickDownEvent.
+ *
+ * @struct JoystickButtonEventArg IJoystick.h Devices/IJoystick.h
+ */
+struct JoystickButtonEventArg {
+    enum JButtonEventType { UNKNOWN, PRESS, RELEASE };
+    JButtonEventType type;
+    JoystickState state;        //!< current joystick state
+    JoystickButton button;      //!< button that triggered the event
+    JoystickButtonEventArg() : button(JBUTTON_NONE) {}
+};
 
-    class IJoystick : public virtual IModule {
-    public:
-	
+class IJoystick : public virtual Core::IModule {
+public:
 	virtual ~IJoystick() {};
-
-	virtual IEvent<JoystickButtonEventArg>& JoystickButtonEvent() = 0;
-	
-	virtual IEvent<JoystickAxisEventArg>& JoystickAxisEvent() = 0;
-    };
+	virtual Core::IEvent<JoystickButtonEventArg>& JoystickButtonEvent() = 0;
+	virtual Core::IEvent<JoystickAxisEventArg>& JoystickAxisEvent() = 0;
+};
 
 }
 }
