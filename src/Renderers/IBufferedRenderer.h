fc1c7d0e2232dff87cf8723704e137e5a6bd8e53
zerny <zerny@openengine.dk>
2008-11-18 22:15:44 +0100

    Added a buffered renderer interface.
    darcs-hash:20081118211544-50fb2-e134e70993fe0f4d89bc359a258a9df15d2cda46.gz

---
+++ src/Renderers/IBufferedRenderer.h
@@ -0,0 +1,37 @@
+// Renderer that contains a buffered texture representation.
+// -------------------------------------------------------------------
+// Copyright (C) 2007 OpenEngine.dk (See AUTHORS) 
+// 
+// This program is free software; It is covered by the GNU General 
+// Public License version 2 or any later version. 
+// See the GNU General Public License for more details (see LICENSE). 
+//--------------------------------------------------------------------
+
+#ifndef _OE_INTERFACE_BUFFERED_RENDERER_H_
+#define _OE_INTERFACE_BUFFERED_RENDERER_H_
+
+#include <Renderers/IRenderer.h>
+#include <Resources/ITextureResource.h>
+
+namespace OpenEngine {
+namespace Renderers {
+
+/**
+ * Renderer that contains a buffered texture representation.
+ *
+ * @class IBufferedRenderer IBufferedRenderer.h Renderers/IBufferedRenderer.h
+ */
+class IBufferedRenderer : public virtual IRenderer {
+public:
+    /**
+     * Get the texture representation of the current color buffer.
+     *
+     * @return Current color buffer
+     */
+    virtual Resources::ITextureResourcePtr GetColorBuffer() const = 0;
+};
+
+} // NS Renderers
+} // NS OpenEngine
+
+#endif // _OE_INTERFACE_BUFFERED_RENDERER_H_
