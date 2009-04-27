88fb0d1227fbcb34c5f08924725d0cf5d9e098f6
salomonhippie <salomonhippie@gmail.com>
2009-04-27 12:38:50 +0200

    Added SelectionList to Utils.
    darcs-hash:20090427103850-79cf9-a3019eef1aeec4909949b8b1810d6b66e6bc57c4.gz

---
+++ src/Utils/SelectionList.h
@@ -0,0 +1,83 @@
+// Selection List.
+// -------------------------------------------------------------------
+// Copyright (C) 2007 OpenEngine.dk (See AUTHORS) 
+// 
+// This program is free software; It is covered by the GNU General 
+// Public License version 2 or any later version. 
+// See the GNU General Public License for more details (see LICENSE). 
+//--------------------------------------------------------------------
+
+#ifndef _OE_SELECTION_LIST_H_
+#define _OE_SELECTION_LIST_H_
+
+#include <Core/IEvent.h>
+#include <Core/Event.h>
+#include <set>
+
+namespace OpenEngine {
+namespace Utils {
+
+using Core::IEvent;
+using Core::Event;
+using std::set;
+
+
+/**
+ * Maintain a selection of objects. Fire events when selection is
+ * updated.
+ *
+ * @class SelectionList SelectionList.h Utils/SelectionList.h
+ */
+template <class T> class SelectionList {
+public:
+    class ChangedEventArg {
+    public:
+        set<T*> selection;
+    };
+private:
+    set<T*> selection;
+    Event<ChangedEventArg> changed;
+public:
+    IEvent<ChangedEventArg>& ChangedEvent() {
+        return changed;
+    }
+
+    void Select(T* obj) {
+        if (selection.insert(obj).second()) {
+            ChangedEventArg arg;
+            arg.selection = selection;
+            changed.Notify(arg);
+        }
+    }
+
+    void Select (set<T*> objs) {
+        selection.insert(objs.begin(), objs.end());
+        ChangedEventArg arg;
+        arg.selection = selection;
+        changed.Notify(arg);
+    }
+
+    void DeSelect(T* obj) {
+        if (selection.erase(obj) == 1) {
+            ChangedEventArg arg;
+            arg.selection = selection;
+            changed.Notify(arg);        
+        }
+    }
+
+    void DeSelect (set<T*> objs) {
+        selection.erase(objs.begin(), objs.end());
+        ChangedEventArg arg;
+        arg.selection = selection;
+        changed.Notify(arg);
+    }
+
+    void Clear() {
+        selection.clear();
+    }
+
+};
+
+}
+}
+#endif
