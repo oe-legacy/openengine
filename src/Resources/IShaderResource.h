7e8b7d3b19b3a7189dad7ac1237d93465866f8e0
asgerhoedt <asgerhoedt@gmail.com>
2010-02-19 11:06:50 +0100

    Added parameter to force binding uniforms and textures to a shader. And added 3D textures to the shader interface
    darcs-hash:20100219100650-ceda8-6340eda7f5d73d741de7a2a628f91648475a4634.gz

--- src/Resources/IShaderResource.h
+++ src/Resources/IShaderResource.h
@@ -22,6 +22,8 @@ namespace Resources {
 // Forward declerations
 class ITexture2D;
 typedef boost::shared_ptr<ITexture2D> ITexture2DPtr;
+class ITexture3D;
+typedef boost::shared_ptr<ITexture3D> ITexture3DPtr;
 
 using OpenEngine::Math::Vector;
 using std::string;
@@ -73,23 +75,31 @@ public:
     virtual void ReleaseShader()=0;
 
     /**
-     * Set a shader uniform.
+     * Set a shader uniform to be bound next time the shader is
+     * applied. Or the user can force it's application, but that
+     * requires the shader to be bound!
      *
      * @param name Uniform name.
      * @param value Uniform vector value.
+     * @param force If true the uniform will be bound immediately,
+     * false will halt the binding until the shader is applied.
      */
-    virtual void SetUniform(string name, int arg) = 0;
-    virtual void SetUniform(string name, float value)=0;
-    virtual void SetUniform(string name, Vector<2, float> value)=0;
-    virtual void SetUniform(string name, Vector<3, float> value)=0;
-    virtual void SetUniform(string name, Vector<4, float> value)=0;
+    virtual void SetUniform(string name, int arg, bool force = false) = 0;
+    virtual void SetUniform(string name, float value, bool force = false)=0;
+    virtual void SetUniform(string name, Vector<2, float> value, bool force = false)=0;
+    virtual void SetUniform(string name, Vector<3, float> value, bool force = false)=0;
+    virtual void SetUniform(string name, Vector<4, float> value, bool force = false)=0;
 
     /**
      * Binds a texture to variable with the name in the shader.
+     *
      * @param name Name to bind to.
      * @param tex Texture to bind.
+     * @param force If true the uniform will be bound immediately,
+     * false will halt the binding until the shader is applied.
      */
-    virtual void SetTexture(string name, ITexture2DPtr tex) = 0;
+    virtual void SetTexture(string name, ITexture2DPtr tex, bool force = false) = 0;
+    virtual void SetTexture(string name, ITexture3DPtr tex, bool force = false) = 0;
 
     /**
      * Get the Textures associated with the shader.
