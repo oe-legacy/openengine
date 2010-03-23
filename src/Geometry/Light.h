cdad4ccf19c78b0645effe263f41aae093ab56b9
salomonhippie <salomonhippie@gmail.com>
2010-03-23 21:23:15 +0100

    added light.h
    Ignore-this: 3a6656df7a626f4873715bd81c63374e
    
    darcs-hash:20100323202315-79cf9-3632031dc9cf9b4c41eff6ff35785b3c09662de6.gz

---
+++ src/Geometry/Light.h
@@ -0,0 +1,60 @@
+// Base class for Light nodes.
+// -------------------------------------------------------------------
+// Copyright (C) 2007 OpenEngine.dk (See AUTHORS) 
+// 
+// This program is free software; It is covered by the GNU General 
+// Public License version 2 or any later version. 
+// See the GNU General Public License for more details (see LICENSE). 
+//--------------------------------------------------------------------
+
+#ifndef _OE_LIGHT_H_
+#define _OE_LIGHT_H_
+
+#include <Math/Vector.h>
+
+#include <boost/serialization/base_object.hpp>
+#include <boost/serialization/utility.hpp>
+#include <boost/serialization/list.hpp>
+#include <boost/serialization/extended_type_info.hpp>
+#include <boost/serialization/export.hpp>
+#include <Meta/BoostSerialization.h>
+
+namespace OpenEngine {
+namespace Scene {
+
+using Math::Vector;
+
+/**
+ * Base class for light nodes.
+ * 
+ * @class Light Light.h Geometry/Light.h
+ */
+class Light {
+public:
+    Vector<4,float> ambient, diffuse, specular;
+
+    Light()
+        : ambient(Vector<4,float>(0.0,0.0,0.0,1.0))
+        , diffuse(Vector<4,float>(1.0,1.0,1.0,1.0))
+        , specular(Vector<4,float>(1.0,1.0,1.0,1.0)) {}
+
+    virtual ~Light() {}
+
+private:
+    friend class boost::serialization::access;
+    template<class Archive>
+    void serialize(Archive & ar, const unsigned int version) {
+        // serialize base class information
+        ar & boost::serialization::base_object<Light>(*this);
+        ar & ambient;
+        ar & diffuse;
+        ar & specular;
+    }
+};
+
+} // NS Scene
+} // NS OpenEngine
+
+BOOST_CLASS_EXPORT(OpenEngine::Scene::Light)
+
+#endif // _OE_LIGHT_H_
