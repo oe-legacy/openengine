74c79c85084a59bb4c75905db03839be28b3ac67
Peter Kristensen <ptx@openengine.dk>
2011-01-12 12:14:12 +0100

    Improved oe-new-file
    Ignore-this: 3b8b09f55ce343848ba8b3f22ba564f5
    
    darcs-hash:20110112111412-1cf9e-5bf90d19fc32abc89215afd927a5b60d574f7a67.gz

--- conf/emacs/openengine.el
+++ conf/emacs/openengine.el
@@ -15,21 +15,54 @@
         ((file-exists-p (concat path "/make.py")) path)
         (t (oe-find-make-path (path-parent path)))))
 
+(defun is-upper (c)
+  (and (>= c ?A)
+       (<= c ?Z)))
+
+(defun camel-to-caps (string)
+  (upcase (apply 'concat (mapcar 
+                  (lambda (ch)
+                    (if (is-upper ch)
+                        (concat "_" (char-to-string ch))
+                      (char-to-string ch)))                    
+                  string))))
+
 (defun oe-new-file (file)
+  ;; todo
+  ;; - only add ifdef in .h files                                    
   (interactive "F")
   (find-file file)
-  (let ((make-path (oe-find-make-path (path-parent (buffer-file-name))))
-        (namespace (car (last (split-string (path-parent file) "/")))))
+  (let* ((make-path (oe-find-make-path (path-parent (buffer-file-name))))
+         (namespace (car (last (split-string (path-parent file) "/"))))
+         (filen (car (last (split-string (buffer-file-name) "/"))))         
+         (dotparts (split-string filen "\\."))
+         (class (car dotparts))
+         (extension (cadr dotparts))         
+         (isheader (string-equal "h" extension))
+         (def-caps (camel-to-caps class)))    
     (when make-path
       ;; insert header
       (insert-file (concat make-path "/conf/HEADER"))
       (end-of-buffer)
-      (insert "\n\n"
+      (insert 
+              "\n\n")
+      (if isheader
+          (insert 
+              "#ifndef _OE" def-caps "_H_\n"
+              "#define _OE" def-caps "_H_\n"
+              "\n\n")
+        )
+      (insert
               "namespace OpenEngine {\n"
               "namespace " namespace " {\n"
               "\n\n"
               "} // NS " namespace "\n" 
-              "} // NS OpenEngine\n")
+              "} // NS OpenEngine\n\n"
+              )
+      (if isheader
+          (insert
+              "#endif // _OE" def-caps "_H_\n"
+              ))
       (beginning-of-buffer)
       (forward-char 3)
       (kill-line))))
@@ -67,24 +100,43 @@
     (if make-path
         ;; we want to close the compile window again if no errors occurred
         ;; just cant find the function right now :/
-	(let ((my-buf (current-buffer))
-	      (com-buf (compile (concat "cd " make-path " && ./make.py " target " -k"))))
-	  (switch-to-buffer-other-window com-buf)
-	  (end-of-buffer)
-	  (switch-to-buffer-other-window my-buf)
-	  )
-        ;; default to ordinary compile
-        (call-interactively 'compile))))
+        (let ((my-buf (current-buffer))
+              (com-buf (compile (concat "cd " make-path " && ./make.py " target " -k"))))
+          (switch-to-buffer-other-window com-buf)
+          (end-of-buffer)
+          (switch-to-buffer-other-window my-buf))          
+      ;; default to ordinary compile
+      (call-interactively 'compile))))
+
+(setq oe-compile-cont
+      nil)
+(setq oe-compile-arg
+      nil)
+
+(defun oe-compile-finished (buf state)
+  (if oe-compile-cont
+      (apply oe-compile-cont (list oe-compile-arg))))
+
+
+(add-to-list 'compilation-finish-functions 'oe-compile-finished)
 
 (defun oe-generate-tags ()
   "Generate a TAGS file for Emacs and add it to the tags table."
   (interactive)
   (let ((make-path (oe-find-make-path (path-parent (buffer-file-name)))))
     (when make-path
-      (oe-compile "etags")
-      (add-to-list 'tags-table-list (concat make-path "/build")))))
+      (setq oe-compile-arg
+            (concat make-path "/build/TAGS"))
+       (setq oe-compile-cont
+             (lambda (file)
+               (visit-tags-table file)
+               (setq oe-compile-cont nil)
+               (setq oe-compile-arg nil)))
+      (oe-compile "etags"))))
 
-(add-hook 'c++-mode-hook
+
+
+(add-hook 'c-mode-common-hook
   (lambda ()
     (local-set-key [(control c) (control v)] 'oe-compile)
     (local-set-key [(control c) (control r)]
