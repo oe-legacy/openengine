360fef450d0ed7f830d45b32368d441331b9dfae
ptx <ptx@openengine.dk>
2011-08-01 12:55:52 +0200

    less depth buffer please
    Ignore-this: adb37b4b95737b94276ac7acf09040f3
    
    darcs-hash:20110801105552-2a962-5b3f3a69af0b231c8a418db88a968766e7d5c4f0.gz

--- src/Display/PerspectiveViewingVolume.h
+++ src/Display/PerspectiveViewingVolume.h
@@ -23,7 +23,7 @@ namespace Display {
 class PerspectiveViewingVolume : public ViewingVolume {
 public:
 
-    PerspectiveViewingVolume(const float distNear = 1,
+    PerspectiveViewingVolume(const float distNear = 10,
                              const float distFar  = 30000,
                              const float aspect   = 4.0/3.0,
                              const float fov      = PI/4.0);
